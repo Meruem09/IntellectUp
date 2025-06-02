3afaafdfb27e6f9e277de9b4f9a39ac7fcfd4620
Rahul Varma <rahulvarma1797@gmail.com>
2025-06-02 15:32:20 +0530

    added api.cjs for routes

---
+++ server/routes/api.cjs
@@ -0,0 +1,25 @@
+// src/api.js
+import axios from "axios";
+const api = axios.create({
+  baseURL: "http://localhost:3001",
+});
+
+// Add interceptor
+export const useApiWithToken = () => {
+  const { getToken } = useAuth();
+
+  const api = axios.create({
+    baseURL: "http://localhost:3001",
+  });
+
+  api.interceptors.request.use(async (config) => {
+    const token = await getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  return api;
+};
+export default api;
