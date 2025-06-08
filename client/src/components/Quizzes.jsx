50cfce92fa6b1fc749250fac02de83d9757f17b4
Rahul Varma <rahulvarma1797@gmail.com>
2025-06-08 22:20:04 +0530

    nothings

--- client/src/components/Quizzes.jsx
+++ client/src/components/Quizzes.jsx
@@ -7,10 +7,7 @@ const Quizzes = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla suscipit perferendis eaque voluptatem mollitia. Cumque asperiores aut itaque dignissimos? Adipisci numquam enim ducimus molestias iure temporibus minima culpa labore cum!
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla suscipit perferendis eaque voluptatem mollitia. Cumque asperiores aut itaque dignissimos? Adipisci numquam enim ducimus molestias iure temporibus minima culpa labore cum!
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla suscipit perferendis eaque voluptatem mollitia. Cumque asperiores aut itaque dignissimos? Adipisci numquam enim ducimus molestias iure temporibus minima culpa labore cum!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla suscipit perferendis eaque voluptatem mollitia. Cumque asperiores aut itaque dignissimos? Adipisci numquam enim ducimus molestias iure temporibus minima culpa labore cum!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla suscipit perferendis eaque voluptatem mollitia. Cumque asperiores aut itaque dignissimos? Adipisci numquam enim ducimus molestias iure temporibus minima culpa labore cum!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla suscipit perferendis eaque voluptatem mollitia. Cumque asperiores aut itaque dignissimos? Adipisci numquam enim ducimus molestias iure temporibus minima culpa labore cum!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla suscipit perferendis eaque voluptatem mollitia. Cumque asperiores aut itaque dignissimos? Adipisci numquam enim ducimus molestias iure temporibus minima culpa labore cum!
+            hii
         </div>
      );
 }
